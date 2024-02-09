42c7853acce42476ddc0079114f241543d6863bf
Rebeca Cañeque Rufo <rebequitarufo@gmail.com>
2024-02-09 21:04:10 +0100

    Mi primer commit

---
+++ Ejemplo/src/Ejemplo/MainApp.java
@@ -0,0 +1,10 @@
+package Ejemplo;
+
+public class MainApp {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("HOLA MUNDOOO");
+	}
+}
---
+++ Ejemplo/src/Ejemplo/otraClase.java
@@ -0,0 +1,11 @@
+package Ejemplo;
+
+public class otraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("si sumamos 2 + 2 es igual a "+(2+2));
+	}
+
+}
